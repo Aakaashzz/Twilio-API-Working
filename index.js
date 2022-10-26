1674352c67d211d2c57732d132f6b0f273b913aa
Aakaashzz <84792076+Aakaashzz@users.noreply.github.com>
2022-10-26 12:14:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const accountSid = "AC7a55fe59e16b137b44056b577c9dd14f";
+const accountSid = "AC7a55fe59e16b137b44056b577c4f";
 const authToken = "5ade83ff4c9e3d1c517651fd22ce53a9";
 const client = require('twilio')(accountSid, authToken);
 
